1435d2c168f44210a74d93a221321d966a6bdeb5
= <=>
2025-01-16 10:37:39 +0100

    fin_site_rajout_page_error404_reste_a_mettre_systeme_trie_recherche_page_inscription_connexion_dans_le_back

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,32 @@ import ProductPage from "./pages/ProductPage";
 import UserCreate from "./components/UserCreate";
 import UserLogin from "./components/UserLogin";
 import Shop from "./pages/Shop";
+import Cart from "./components/Cart";
+import Error404 from "./pages/Error404";
 import { Box } from "@mui/material";
+import { CartProvider } from "./components/CartContext";
 
 function App() {
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-      <Router>
-        <Navbar />
-        <Box sx={{ flex: 1 }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/product" element={<ProductPage />} />
-            <Route path="/create-account" element={<UserCreate />} />
-            <Route path="/login" element={<UserLogin />} />
-            <Route path="/shop" element={<Shop />} />
-          </Routes>
-        </Box>
-        <Footer />
-      </Router>
-    </Box>
+    <CartProvider>
+      <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <Router>
+          <Navbar />
+          <Box sx={{ flex: 1 }}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/product" element={<ProductPage />} />
+              <Route path="/create-account" element={<UserCreate />} />
+              <Route path="/login" element={<UserLogin />} />
+              <Route path="/shop" element={<Shop />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="*" element={<Error404 />} />
+            </Routes>
+          </Box>
+          <Footer />
+        </Router>
+      </Box>
+    </CartProvider>
   );
 }
 
